1524fd262efd770d164716f03e35eff85e69542e
fauzi <fauziilyassaputra@gmail.com>
2024-08-28 22:15:00 +0700

    hilangin titik

--- script.js
+++ script.js
@@ -25,21 +25,21 @@ const tombol5 = document.querySelector(".tombol5");
 
 tombol1.addEventListener("click", (event) => {
   komentar.innerHTML =
-    '<img src="komentator/matt.png" alt=""> <p class="komentar"><span>Matt Journey</span> Masakan khas madura yang susah untuk tidak mampir jika melewati jalan arah ke Pamekasan (jika dari arah barat) atau jalan arah ke Sampang (jika dari arah timur)</p>';
+    '<img src="komentator/matt.png" alt=""><p class="komentar"><span>Matt Journey</span><Masakan khas madura yang susah untuk tidak mampir jika melewati jalan arah ke Pamekasan (jika dari arah barat) atau jalan arah ke Sampang (jika dari arah timur)</p>';
 });
 tombol2.addEventListener("click", (event) => {
   komentar.innerHTML =
-    '<img src="komentator/puteri.png" alt=""> <p class="komentar"><span>Puteri Qotrina</span> Makanan seafood enak bumbu rumahan harga bersahabat banget. Potongan ikannya gede2. Fresh. Ada jg non seafood. RM oldschool tp tetap survive Krn RASA </p>';
+    '<img src="komentator/puteri.png" alt="">                                                                           <p class="komentar"><span>Puteri Qotrina</span> Makanan seafood enak bumbu rumahan harga bersahabat banget. Potongan ikannya gede2. Fresh. Ada jg non seafood. RM oldschool tp tetap survive Krn RASA </p>';
 });
 tombol3.addEventListener("click", (event) => {
   komentar.innerHTML =
-    '<img src="komentator/arifin.png" alt=""> <p class="komentar"><span>arifin waluya</span>............................................................ Tempat ini easy to notice Masakannya cocok dengan lidah saya lidah jawa harga Alhamdulillah bersahabat</p>';
+    '<img src="komentator/arifin.png" alt="">                                                                           <p class="komentar"><span>arifin waluya</span> Tempat ini easy to notice Masakannya cocok dengan lidah saya lidah jawa harga Alhamdulillah bersahabat</p>';
 });
 tombol4.addEventListener("click", (event) => {
   komentar.innerHTML =
-    '<img src="komentator/rezeki.png" alt=""> <p class="komentar"><span>Awal M Rezeki</span> ............................................................... Harga murah, rasa enak, variasi menu banyak, lokasi strategis, tempat bersih dan luas... Yg terpenting lagi penjualnya ramah</p>';
+    '<img src="komentator/rezeki.png" alt="">                                                                           <p class="komentar"><span>Awal M Rezeki</span> Harga murah, rasa enak, variasi menu banyak, lokasi strategis, tempat bersih dan luas... Yg terpenting lagi penjualnya ramah</p>';
 });
 tombol5.addEventListener("click", (event) => {
   komentar.innerHTML =
-    '<img src="komentator/saifudin.png" alt=""> <p class="komentar"><span>Saifudin Gituloh</span>...disediakan tempat lesehan maupun duduk. Untuk harga cukup murah.... Waktu saya berkunjung disini tidak ada tukang parkir sehingga bebas parkir.</p>';
+    '<img src="komentator/saifudin.png" alt="">                                                                         <p class="komentar"><span>Saifudin Gituloh</span>...disediakan tempat lesehan maupun duduk. Untuk harga cukup murah.... Waktu saya berkunjung disini tidak ada tukang parkir sehingga bebas parkir.</p>';
 });
